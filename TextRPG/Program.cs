ad9531db6c3f818798c5d8da83bd4e4fbe20b7e5
jihyeog144 <jiheog144@gmail.com>
2025-04-01 14:35:13 +0900

    초기 커밋.

---
+++ TextRPG/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace TextRPG
+{
+    internal class Program
+    {
+        public static class Game
+        {
+            //게임에 필요한 정보들
+
+            // 게임에 필요한 기능들
+            // 1. 게임시작
+
+
+            private static bool gameOver;
+
+            public static void Start()
+            {
+
+            }
+            // 2. 게임 종료
+            public static void End()
+            {
+
+            }
+            // 3. 게임 동작
+            public static void Run()
+            {
+                while (gameOver == false)
+                {
+
+                }    
+            }
+
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Game.Start();
+            Game.Run();
+            Game.End();
+        }
+    }
+}
